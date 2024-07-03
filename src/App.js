64375eaee4de35b70b5b6644fafba15bc8b96a8c
Dinesh yadav <dineshyadav74950@gmail.com>
2024-07-04 00:25:29 +0530

    some minnor changes applied here

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { getAuth,createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 import {app} from './firebase';
 import SignupPage from './pages/Signup';
 import SigninPage from './pages/Signin';
@@ -8,6 +12,27 @@ const auth = getAuth(app);
 
 function App() {
 
+  const [user,setUser]=useState(null);
+  useEffect(() =>{onAuthStateChanged(auth,user=>{
+    if (user){
+      //yes you are logged in 
+      console.log("User",user);
+      setUser(user);
+    }else{
+      //user is not logged out
+      console.log("User is not logged out");
+      setUser(null);
+    }
+  })},[]);
+
+  if(user==null){
+    return(
+      <div className='App'>
+        <SignupPage/>
+        <SigninPage/>
+      </div>
+    )
+  }
   const signupUser = () =>{
     createUserWithEmailAndPassword(auth,
       'dineshyadav74950@gmail.com',
@@ -15,8 +40,8 @@ function App() {
   }
   return (
     <div className='App'>
-       <SignupPage/>
-       <SigninPage/>
+       <h1>Hello {user.email}</h1>
+       <button onClick={()=>signOut(auth)}>Logout</button>
     </div>
   );
 }
